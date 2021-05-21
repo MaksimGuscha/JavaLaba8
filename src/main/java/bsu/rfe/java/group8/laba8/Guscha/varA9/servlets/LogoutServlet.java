ea21ee7e9cc4cbefaf64ecb4d1f991be61896b4c
unknown <usermaksim.gushha@mail.ru>
2021-05-21 23:36:57 +0300

    Create classServlet LogoutServlet and classServlet MessageListServlet

---
+++ src/main/java/bsu/rfe/java/group8/laba8/Guscha/varA9/servlets/LogoutServlet.java
@@ -0,0 +1,34 @@
+package bsu.rfe.java.group8.laba8.Guscha.varA9.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import bsu.rfe.java.group8.laba8.Guscha.varA9.entity.ChatUser;
+
+@WebServlet(name = "LogoutServlet")
+public class LogoutServlet extends ChatServlet {
+
+    private static final long serialVersionUID = 1L;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = (String) request.getSession().getAttribute("name");
+        if (name!=null) {
+            ChatUser aUser = activeUsers.get(name);
+            if (aUser!=null && aUser.getSessionId().equals((String)request.getSession().getId())) {
+                synchronized (activeUsers) {
+                    activeUsers.remove(name);
+                }
+                request.getSession().setAttribute("name", null);
+                response.addCookie(new Cookie("sessionId", null));
+                response.sendRedirect(response.encodeRedirectURL("/JavaLaba8/"));
+            } else {
+                response.addCookie(new Cookie("sessionId", null));
+                response.sendRedirect(response.encodeRedirectURL("/JavaLaba8/view.html"));
+            }
+        } else {
+            response.sendRedirect(response.encodeRedirectURL("/JavaLaba8/view.html"));
+        }
+    }
+}
